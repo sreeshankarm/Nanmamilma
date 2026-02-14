9cf6ffb06c1dfd9b576e0e1b84f66793b09f8478
sreeshankarm <sreeshankar.m@duk.ac.in>
2026-02-14 17:44:31 +0530

    added files

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,41 @@
 
 
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [["babel-plugin-react-compiler"]],
+//       },
+//     }),
+//     tailwindcss(),
+//   ],
+
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://nanmastagingapi.milma.in",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Nanmamilma/", // 👈 ADD THIS (your repo name)
+
   plugins: [
     react({
       babel: {
@@ -46,4 +76,3 @@ export default defineConfig({
     },
   },
 });
-
